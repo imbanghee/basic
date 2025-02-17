10e254ca386ad2c374cc9bf2d3f64195d05c84d3
bbang0123 <bbang0@naver.com>
2025-02-17 16:42:32 +0900

    modify 2

--- demo/src/main/java/com/example/demo/restapi/service/MyService.java
+++ demo/src/main/java/com/example/demo/restapi/service/MyService.java
@@ -11,7 +11,7 @@ public class MyService {
 
 	@GetMapping("/myService")
 	public String myService() {
-		return "response restController intellij modify>>>>>>";
+		return "response restController intellij modify 2";
 	}
 	
 }
