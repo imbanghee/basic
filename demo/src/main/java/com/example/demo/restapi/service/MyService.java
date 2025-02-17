3cf3867a8e9a16619f54afd7419a118329fc90e9
imbanghee <imbanghee@gmail.com>
2025-02-17 17:11:42 +0900

    주석없앰

--- demo/src/main/java/com/example/demo/restapi/service/MyService.java
+++ demo/src/main/java/com/example/demo/restapi/service/MyService.java
@@ -1,6 +1,5 @@
 package com.example.demo.restapi.service;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
